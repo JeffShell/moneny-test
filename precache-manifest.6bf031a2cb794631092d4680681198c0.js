1a92cb33c835066904dfc0c326f369309310a7e9
jeffshell <875899952@qq.com>
2021-07-15 00:43:54 +0800

    deploy

---
+++ precache-manifest.6bf031a2cb794631092d4680681198c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f546903ae55d5e38c8ff",
+    "url": "css/app.7f9b77fe.css"
+  },
+  {
+    "revision": "0a3faac4677424a5cf58a93feaffb917",
+    "url": "index.html"
+  },
+  {
+    "revision": "f546903ae55d5e38c8ff",
+    "url": "js/app.376c76de.js"
+  },
+  {
+    "revision": "f8531c0199d8b3c15a51",
+    "url": "js/chunk-vendors.5180712d.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
